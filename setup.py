f73acb93a24a748e572557e231d3defdd27066b8
James Arthur <thruflo@googlemail.com>
2009-03-13 09:11:11 +0000

    basic egg structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'py-soocial',
+    version = '0.1',
+    description = "Python bindings for the http://www.soocial.com developer API",
+    author = 'thruflo',
+    author_email = 'thruflo@googlemail.com',
+    url = 'http://thruflo.github.com',
+    packages = find_packages('src'),
+    package_dir = {
+        '': 'src'
+    },
+    include_package_data = True,
+    zip_safe = False,
+    dependency_links = [
+        'http://pypi.python.org/simple'
+    ],
+    install_requires = [],
+    entry_points = {}
+)
